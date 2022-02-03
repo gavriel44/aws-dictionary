356061f3479d4bee0108b3a3b693d491a3411d28
Gavriel Frant <gavril44@gmail.com>
2022-02-03 19:15:28 +0200

    added cores support

--- aws-node-express-dynamodb-api-project/app.js
+++ aws-node-express-dynamodb-api-project/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const wordRouter = require("./routers/wordRouter");
 const partOfSpeechRouter = require("./routers/partOfSpeechRouter");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/word", wordRouter);
@@ -11,7 +13,7 @@ app.use("/partOfSpeech", partOfSpeechRouter);
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    error: "Not Found sorry! and go",
   });
 });
 
