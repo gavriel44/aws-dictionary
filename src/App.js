5d9de3ea6da1c83503cc61dc3de1e114228d425c
Gavriel Frant <gavril44@gmail.com>
2022-02-14 22:59:30 +0200

    added react query for client side caching

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import SearchAppBar from "./components/SearchAppBar";
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [searchHistory, setSearchHistory] = useState({});
   return (
     <div className="App">
-      <SearchAppBar />
-      <Outlet context={[searchHistory, setSearchHistory]} />
+      <QueryClientProvider client={queryClient}>
+        <SearchAppBar />
+        <Outlet context={[searchHistory, setSearchHistory]} />
+      </QueryClientProvider>
     </div>
   );
 }
--- src/components/WordBlock.js
+++ src/components/WordBlock.js
@@ -1,13 +1,30 @@
+import axios from "axios";
 import React, { useEffect } from "react";
+import { useQuery } from "react-query";
 import { useOutletContext, useParams } from "react-router-dom";
 import { useDictionaryFetch } from "../hooks/useDictionaryFetch";
+import { capitalizeFirstLetter } from "../utils/help";
 import WordDefinition from "./WordDefinition";
 
 export default function WordBlock() {
   // eslint-disable-next-line no-unused-vars
   const [searchHistory, setSearchHistory] = useOutletContext();
   const { word: currentWord } = useParams();
-  const { wordDefinition, isLoading } = useDictionaryFetch(currentWord);
+  // const { wordDefinition, isLoading } = useDictionaryFetch(currentWord);
+  const capitalizedWord = capitalizeFirstLetter(currentWord);
+  const {
+    isLoading,
+    error,
+    data: wordDefinition,
+  } = useQuery(`${currentWord}Data`, () =>
+    axios
+      .get(
+        `https://v8pauve0t1.execute-api.us-east-1.amazonaws.com/word/${capitalizedWord}`
+      )
+      .then((res) => {
+        return res.data;
+      })
+  );
 
   useEffect(() => {
     setSearchHistory((previousHistory) => {
